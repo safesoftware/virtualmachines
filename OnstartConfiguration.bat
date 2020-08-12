51142a4f2aad50f3eb9e98258efd6bbd668519fa
Ryan Cragg <rjcragg@gmail.com>
2020-08-12 13:03:52 -0700

    fix server authoring manual url

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -35,7 +35,7 @@
 		echo URL=https://knowledge.safe.com/articles/116766/  >>"c:\users\public\desktop\Advanced Attribute and List Handling.url"
 		
 		echo [InternetShortcut] > "c:\users\public\desktop\FME Server Authoring Manual.url"
-		echo URL=https://s3.amazonaws.com/gitbook/Server-Authoring-2020/index.html  >>"c:\users\public\desktop\FME Server Authoring Manual"
+		echo URL=https://s3.amazonaws.com/gitbook/Server-Authoring-2020/index.html  >>"c:\users\public\desktop\FME Server Authoring Manual.url"
 goto :eof
 
 
