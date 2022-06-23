3c5bd0cd230657456b363e6bac2e1609c4a358d5
Ryan Cragg <rjcragg@gmail.com>
2022-06-22 22:30:39 -0700

    Update OnstartConfiguration.bat

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -105,6 +105,7 @@ goto :eof
 	:: download and install the current FMEData from www.safe.com/download
 	:: aria2c https://raw.githubusercontent.com/safesoftware/virtualmachines/strigo/FMEInstalls/FMEDataDownloadInstall.bat --out=FMEDataDownloadInstall.bat --allow-overwrite=true
 	:: CALL FMEDataDownloadInstall.bat
+	pushd %TEMP%
 	aria2c https://s3.amazonaws.com/FMEData/FMEData2022.zip --allow-overwrite=true
 	:: Unzip FMEData
 	for %%f in (FMEDATA*.zip) do 7z x -oc:\ -aoa %%f
