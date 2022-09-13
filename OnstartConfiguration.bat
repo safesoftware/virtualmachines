82607dcd454599e0d18314e653459248853fa6dd
Ryan Cragg <rjcragg@gmail.com>
2022-09-13 10:12:20 -0700

    Update OnstartConfiguration.bat

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -15,8 +15,8 @@
    if %date:~4%==%ESRIDATE% call :esri >>%LOG%
    call :emptyRecycleBin >>%LOG%
    call :urls >>%LOG%
-   call :fmeserverhoops >>%LOG%
    call :fmedatadownload >>%LOG%
+   call :fmeserverhoops >>%LOG%
 
 
 
