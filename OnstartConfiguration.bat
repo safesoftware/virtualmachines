2cb9d0357177a627fefe596ca4495a718e8736eb
Ryan Cragg <ryan.cragg@safe.com>
2024-12-30 10:20:58 -0800

    Update OnstartConfiguration.bat

--- OnstartConfiguration.bat
+++ OnstartConfiguration.bat
@@ -61,7 +61,7 @@ goto :eof
 	:: aria2c https://raw.githubusercontent.com/safesoftware/virtualmachines/strigo/LicenseArcGIS.bat --out=LicenseArcGIS.bat --allow-overwrite=true
 	:: copy LicenseArcGIS.bat c:\users\public\desktop\ /Y
 	:: aria2c https://s3.amazonaws.com/FMETemp/Server_July.fmelic --dir="c:\ProgramData\Safe Software\FME Server\licenses" --out=fme_server.fmelic --allow-overwrite=true
-	aria2c https://s3.amazonaws.com/FMETemp/Server_December.fmelic --dir="c:\ProgramData\Safe Software\FMEFlow\licenses" --out=fme_server.fmelic --allow-overwrite=true
+	aria2c https://s3.amazonaws.com/FMETemp/Server_January.fmelic --dir="c:\ProgramData\Safe Software\FMEFlow\licenses" --out=fme_server.fmelic --allow-overwrite=true
 	
 	:: CALL "C:\Program Files\FMEServer\Server\WindowsService\restartFMEServerWindowsService.bat"
 	:: CALL "C:\Program Files\FMEFlow\Server\WindowsService\restartFMEServerWindowsService.bat"
